0410af812350d846dfecb87a9d35b1df81d17524
Edward <edwardzhang0426@gmail.com>
2024-05-01 13:04:52 +1000

    S3 ready branch from Edward

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import { LoginCallback, Security, SecureRoute } from '@okta/okta-react';
 import Home from './Home';
 import Profile from './Profile';
 import Register from './Register';
-import configData from './config/config.json'
+import AWS from "aws-sdk"
+import axios from 'axios';
 
-
-const oktaAuth = new OktaAuth({
-  issuer: configData.APP_ISSUER,
-  clientId: configData.CLIENTID,
-  redirectUri: window.location.origin + '/login/callback'
-});
-
-
+var APP_ISSUER="";
+var CLIENTID="";
+var oktaAuth = null;
 
 class App extends Component {
+  state = {
+    isLoading: true
+  }
   async componentDidMount() {
     try {
       const response = await fetch('http://localhost:3000/profile', {
@@ -33,38 +32,76 @@ class App extends Component {
     }
   }
 
+  async fetch(){
+    AWS.config.update({
+      accessKeyId: 'AKIAWUNLQXA736SZEFWN',
+      secretAccessKey: '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',
+      region: 'ap-southeast-2'
+    })
+    const s3 = new AWS.S3();
+    const params = {
+      Bucket: 'demoappkpmg',
+      Key: 'config.json',
+      Expires : 3600
+    }
+
+    const url = s3.getSignedUrl('getObject',params);
+    await axios.get(url)
+    .then(response => {
+      APP_ISSUER=response.data.APP_ISSUER;
+      CLIENTID=response.data.CLIENTID;
+      oktaAuth = new OktaAuth({
+        issuer: APP_ISSUER,
+        clientId: CLIENTID,
+        redirectUri: window.location.origin + '/login/callback'
+      });
+      //console.log("In FetchFunction"+this.state.isLoading)
+      if(this.state.isLoading==true){
+        this.setState({isLoading:false})
+      }
+    })
+  }
+
   constructor(props) {
     super(props);
+    this.fetch();
     this.restoreOriginalUri = async (_oktaAuth, originalUri) => {
-      props.history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
-    };
-    //this.customAuthHandler = this.customAuthHandler.bind(this);
+        props.history.replace(toRelativeUrl(originalUri || '/', window.location.origin));};
   }
-  // customAuthHandler = async () =>{
-  //   alert("You are not authenticated!");
-  //   const {history} = this.props;
-  //   history.push('/');
-  // }
+    
+    //this.customAuthHandler = this.customAuthHandler.bind(this);
+  
+  customAuthHandler = async () =>{
+    alert("You are not authenticated!");
+    const {history} = this.props;
+    history.push('/');
+  }
 //onAuthRequired={this.customAuthHandler} component={LoginCallback}
   //render={ (props) => <LoginCallback {...props} onAuthResume={ this.onAuthResume } /> }
+  
   render() {
-    return (
-      <Security oktaAuth={oktaAuth} restoreOriginalUri={this.restoreOriginalUri} onAuthRequired={this.customAuthHandler}>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/register" exact={true} component={Register}/>
-        <Route path="/login/callback"  render= { (props) => {
-          console.log(JSON.stringify( props.history.location) );
-          const error = new URLSearchParams(props.history.location.search).get("error");
-          console.log("Print: "+error);
-          if(error!=null){
-            alert("Access denied please log in again");
-            return <Redirect to="/"/>
-          }
-          return <LoginCallback {...props}/> }
-        }/>
-        <SecureRoute path="/profile" component={Profile}/>
-      </Security>
-    );
+    if(this.state.isLoading==true){
+      return "Loading...";
+    }else{
+      console.log("Rendering...")
+      return (
+        <Security oktaAuth={oktaAuth} restoreOriginalUri={this.restoreOriginalUri} onAuthRequired={this.customAuthHandler}>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/register" exact={true} component={Register}/>
+          <Route path="/login/callback"  render= { (props) => {
+            console.log(JSON.stringify( props.history.location) );
+            const error = new URLSearchParams(props.history.location.search).get("error");
+            console.log("Print: "+error);
+            if(error!=null){
+              alert("Access denied please log in again");
+              oktaAuth.signInWithRedirect();
+            }
+            return <LoginCallback {...props}/> }
+          }/>
+          <SecureRoute path="/profile" component={Profile}/>
+        </Security>
+      );
+    } 
   }
 }
 
